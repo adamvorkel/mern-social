251b9b4fbd3fb04332cd78bcecff94ccd4011988
Adam Vorkel <adamvorkel@gmail.com>
2020-09-22 18:37:43 +0200

    added config, db connection and docker-compose with mongo dev container

---
+++ api/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+const db = config.get('mongoURI');
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const connect = async () => {
+  try {
+    await mongoose.connect(db, options);
+    console.log(`Connected to database at URI ${db}`);
+  } catch (error) {
+    console.error(`Failed to connect to database at URI ${db}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connect;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const connect = require('./db');
 
 const app = express();
+
+// connect to db
+connect();
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server started at ${PORT}`);
+  console.log(`Server started at port ${PORT}`);
 });
