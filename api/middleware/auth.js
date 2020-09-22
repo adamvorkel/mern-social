33d3a3e9cbe3ec915765fc0b45272486be52977f
Adam Vorkel <adamvorkel@gmail.com>
2020-09-22 21:36:50 +0200

    added authentication middleware

---
+++ api/middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+const jwtSecret = config.get('jwtSecret');
+
+const auth = (req, res, next) => {
+  const token = req.header('x-auth-token');
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, failed authorization' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, jwtSecret);
+    req.user = decoded.user;
+    next();
+  } catch (error) {
+    res.status(401).json({ msg: 'Invalid token' });
+  }
+};
+
+module.exports = auth;
