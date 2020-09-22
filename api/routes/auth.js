c91ee55fd187b09df900226a3f16502310249128
Adam Vorkel <adamvorkel@gmail.com>
2020-09-22 21:41:09 +0200

    added protected auth route to get user data

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
+const User = require('../models/User');
+
 // @route GET api/auth
-// @desc Test route
-// @access public
-router.get('/', (req, res) => {
-  res.send('Auth route');
+// @desc User data route
+// @access protected
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    return res.json(user);
+  } catch (error) {
+    console.error(err.message);
+    return res.status(500).send({ msg: 'Server error' });
+  }
 });
 
 module.exports = router;
