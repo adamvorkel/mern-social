75536281b31b6348455e4a79e66bf5b0c48774e0
Adam Vorkel <adamvorkel@gmail.com>
2020-09-23 16:07:44 +0200

    added route to allow loggen in user to create a post

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route GET api/posts
-// @desc Test route
-// @access public
-router.get('/', (req, res) => {
-  res.send('Posts route');
-});
+const User = require('../models/User');
+const Profile = require('../models/Profile');
+const Post = require('../models/Post');
+const auth = require('../middleware/auth');
+
+// @route POST api/posts
+// @desc Create a post
+// @access protected
+router.post(
+  '/',
+  [auth, [check('body', 'Body if required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const newPost = new Post({
+        body: req.body.body,
+        user: req.user.id,
+        username: user.name,
+      });
+      const post = await newPost.save();
+      return res.json(post);
+    } catch (error) {
+      console.error(error.message);
+      return res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
