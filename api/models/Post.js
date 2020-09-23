5c96fd56f64dc94f0ac0dc23a40c863d39220eea
Adam Vorkel <adamvorkel@gmail.com>
2020-09-23 15:49:52 +0200

    added Post mongoose model

---
+++ api/models/Post.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  likes: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users',
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users',
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Post = mongoose.model('post', PostSchema);
+
+module.exports = Post;
