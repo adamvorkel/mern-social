acde639d318aa06b56f4ad0e3d4f2f9f69a16df3
Adam Vorkel <adamvorkel@gmail.com>
2020-09-30 23:43:39 +0200

    setup redux store boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+import store from './store';
 import Navbar from './components/layout/Navbar';
 import LandingView from './components/views/LandingView';
 import LoginView from './components/views/LoginView';
@@ -9,16 +11,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <Router>
-        <Navbar />
-        <Route exact path='/' component={LandingView} />
-        <Switch>
-          <Route exact path='/login' component={LoginView} />
-          <Route exact path='/register' component={RegisterView} />
-        </Switch>
-      </Router>
-    </div>
+    <Provider store={store}>
+      <div className='App'>
+        <Router>
+          <Navbar />
+          <Route exact path='/' component={LandingView} />
+          <Switch>
+            <Route exact path='/login' component={LoginView} />
+            <Route exact path='/register' component={RegisterView} />
+          </Switch>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
